b05523c08c4b258f6518aa3fa52e2742c08f738b
Ayush Bansal <bansalayuayu151185@gmail.com>
2026-02-23 00:33:52 +0530

    "Transaction Created"

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -2,11 +2,13 @@ const transactionModel = require("../models/transaction.model");
 const ledgerModel = require("../models/ledger.model");
 const accountModel = require("../models/account.model");
 const emailService = require("../services/email.service");
+const { json } = require("express");
+const mongoose = require("mongoose");
 
 async function createTransaction(req,res) {
 
     /**
-     * 1. Validaye request
+     * 1. Validate request
      */
     const {fromAccount, toAccount, amount, idempotencyKey} = req.body
 
@@ -35,5 +37,110 @@ async function createTransaction(req,res) {
      */
 
 
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey: idempotencyKey
+    })
+
+    if(isTransactionAlreadyExists){
+        if(isTransactionAlreadyExists.status === 'COMPLETED'){
+            return res.status(200).json({
+                message: "Transaction already processed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === 'PENDING'){
+            return res.status(200).json({
+                message: "Transaction is still processing",
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === 'FAILED'){
+            return res.status(500).json({
+                message: "Transaction processing failed previously, please retry"
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === 'REVERSED'){
+            return res.status(500).json({
+                message: "Transaction was reversed, please retry"
+            })
+        }
+    }
+
+
+    /**
+     * 3. Check account status
+     */
+
+    if(fromUserAccount.status !== 'ACTIVE' || toUserAccount.status !== 'ACTIVE'){
+        return res.status(400).json({
+            message: "Both fromAccount and toAccount must be ACTIVE to process transaction"
+        })
+    }
+
+    /**
+     * 4. Derive sender balance from ledger
+     */
+    const balance = await fromUserAccount.getBalance()
+
+    if(balance < amount){
+        res.status(400).json({
+            message: `Insufficient balance, Current balance is ${balance}. Requested amount is ${amount}`
+        })
+    }
+
+    /**
+     * 5. Create Transaction (PENDING)
+     */
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
+    const transaction = await transactionModel.create({
+        fromAccount,
+        toAccount,
+        amount,
+        idempotencyKey,
+        status: "PENDING"
+    }, {session})
+
+    const debitLedgerEntry = await ledgerModel.create({
+        account: fromAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "DEBIT"
+    }, {session})
+
+    const creditLedgerEntry = await ledgerModel.create({
+        account: toAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "CREDIT"
+    }, {session})
+
+    transaction.status = "COMPLETED"
+
+    await transaction.save({session})
+
+    await session.commitTransaction()
+    session.endSession()
+
+
+    /**
+     * Send Email Notification
+     */
+
+    await emailService.sendTransactionEmail(req.user.email, req.user.name,amount,toAccount)
+    return res.status(201).json({
+        message: "Transaction completed successfully",
+        transaction: transaction
+    })
+
     
+}
+
+
+module.exports = {
+    createTransaction
 }
--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,6 +1,6 @@
 const { Router} = require("express");
 const authMiddleware = require("../middleware/auth.middleware");
-
+const transactionController = require("../controllers/transaction.controller")
 
 
 const transactionRoutes = Router();
@@ -11,6 +11,6 @@ const transactionRoutes = Router();
  * - Create a new transaction
  */
 
-transaction.post("/", authMiddleware.authMiddleware)
+transaction.post("/", authMiddleware.authMiddleware,transactionController.createTransaction);
 
 module.exports = transactionRoutes;
